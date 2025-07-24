bca007d9b3bc4449c8118d018c9df5d529d070ed
tosif121 <tosifraza8867@gmail.com>
2025-07-24 20:27:18 +0530

    routes

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  env: {
-    CLIENT_ID: 'TEST-M22H5VMQSVGIC_25041',
-    CLIENT_SECRET: 'MzlhMWZhMzItNDBhMS00OTYzLWI1OWMtMjFmOTlhNzFlMGFl',
-    CLIENT_VERSION: '1',
-  },
   output: 'export',
   distDir: 'out',
   images: {
