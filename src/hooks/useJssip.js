61ff4fc0260447ab7463501895867ae0eee2aa94
tosif121 <tosifraza8867@gmail.com>
2025-09-19 16:06:37 +0530

    reports

--- src/hooks/useJssip.js
+++ src/hooks/useJssip.js
@@ -53,7 +53,7 @@ const useJssip = (isMobile = false) => {
     autoStart: false,
   });
 
-  const [origin, setOrigin] = useState('devapp.iotcom.io');
+  const [origin, setOrigin] = useState('esamwad.iotcom.io');
 
   useEffect(() => {
     const originWithoutProtocol = window.location.origin.replace(/^https?:\/\//, '');
@@ -1002,7 +1002,7 @@ const useJssip = (isMobile = false) => {
     }
     const initializeJsSIP = () => {
       try {
-        var socket = new JsSIP.WebSocketInterface(`wss://${origin}:8099/ws`);
+        var socket = new JsSIP.WebSocketInterface(`wss://${origin}:8089/ws`);
 
         // Add direct socket error handling
         socket.onclose = function (event) {
@@ -1024,7 +1024,7 @@ const useJssip = (isMobile = false) => {
         var configuration = {
           sockets: [socket],
           session_timers: false,
-          uri: `${username.replace('@', '-')}@${origin}:8099`,
+          uri: `${username.replace('@', '-')}@${origin}:8089`,
           password: password,
         };
 
