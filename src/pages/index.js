328db22befcbe43a5ec2ae50c8b90427263e84b0
tosif121 <tosifraza8867@gmail.com>
2025-12-18 15:28:14 +0530

    /pre fix add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-function index() {
-  return (
-    <div>index</div>
-  )
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/mobile/login');
+    }
+  }, [router]);
+
+  return <MobileTabsWrapper />;
 }
-
-export default index
